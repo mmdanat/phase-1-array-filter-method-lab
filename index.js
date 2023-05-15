980d01e4cf66d536479281454c358eda14cc5273
Morgan MDanat <mdanatmorgan3@gmail.com>
2023-05-14 20:04:02 -0600

    completed filter lab

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Code your solution here
+
+
+function findMatching(listOfDriversNames, nameToFind){
+    const matchingNames = listOfDriversNames.filter((driverName) => {
+        if (driverName == nameToFind ){
+            return true;
+        } else if (driverName.toLowerCase() == nameToFind.toLowerCase()){
+            return true;
+        }
+        
+    });
+    return matchingNames
+}
+
+    function fuzzyMatch(driversNames,lettersToMatch){
+
+    let findMatchingLetters = driversNames.filter((individualNames) => {
+        if ( lettersToMatch.charAt(0) === individualNames.charAt(0)){
+            return true;
+        }});
+        return findMatchingLetters;
+   
+}
+
+
+
+
+function matchName(drivers,nametoMatch){
+   
+    // let objectName = drivers.name
+
+    let findmatchingName2 = drivers.filter((individualNames) =>{
+       let objectName = individualNames.name
+        if (objectName == nametoMatch){
+            return true;
+        }})
+
+        return findmatchingName2;
+}
+
+// console.log(matchName(drivers,"Bobby"))
+
+
+
+
+
+// console.log((fuzzyMatch(drivers, 'Sa')))
+
+
+
+
+
+
+
+
